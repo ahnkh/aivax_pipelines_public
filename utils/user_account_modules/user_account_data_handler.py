dd669deba684e6f1a780d15fc6a0f57319738a98
khan <acf21s@gmail.com>
2026-06-09 13:45:17 +0900

    사용자 계정 미등록 현상, 재시도 로직 추가

--- utils/user_account_modules/user_account_data_handler.py
+++ utils/user_account_modules/user_account_data_handler.py
@@ -287,6 +287,8 @@ class UserAccountDataHandler:
         #신규 사용자가 등록되면, email과 서비스 id를 지정한다.
         #우선 테스트
         
+        nErrorCode:int = dictDBResult.get(DBSQLDefine.QUERY_ERR_CODE)
+        
         default_server_ip:str = dictDBApiServer.get("default_server_ip")
         default_server_port:str = dictDBApiServer.get("default_server_port")
         default_schema:str = dictDBApiServer.get("default_schema")
@@ -298,6 +300,11 @@ class UserAccountDataHandler:
         }
         
         self.__requestToSimpleJsonRequest(strURL, post)
+
+        # 응답값의 반환        
+        if 0 != nErrorCode:
+            LOG().error(f"fail insert user account, mail = {email}, service = {ai_service}")
+            return ERR_FAIL
         
         return ERR_OK
         
